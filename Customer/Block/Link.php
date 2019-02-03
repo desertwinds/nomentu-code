43fd49bb026348bbe9160ff9069cb2821cb6d30a
Javier <javiantonini@gmail.com>
2019-02-03 14:18:02 +0000

    Fixed bug my account link only showing on customer site.

--- Customer/Block/Link.php
+++ Customer/Block/Link.php
@@ -2,24 +2,26 @@
 
 namespace Nomentu\Customer\Block;
 
+use Magento\Customer\Model\Context;
+
 class Link extends \Magento\Customer\Block\Account\Link
 {
-    protected $_customerSession;
+    protected $_httpContext;
 
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
-        \Magento\Customer\Model\Session $customerSession,
+        \Magento\Framework\App\Http\Context $httpContext,
         \Magento\Customer\Model\Url $customerUrl,
         array $data = []
      ) {
-         $this->_customerSession = $customerSession;
+         $this->_httpContext = $httpContext;
          parent::__construct($context, $customerUrl, $data);
      }
 
     protected function _toHtml()
     {    
         $responseHtml = null; //  need to return at-least null
-        if($this->_customerSession->isLoggedIn()) {
+        if($this->_httpContext->getValue(Context::CONTEXT_AUTH)) {
             $responseHtml = parent::_toHtml();
         }
         return $responseHtml;
