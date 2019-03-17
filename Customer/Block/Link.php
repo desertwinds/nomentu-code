da1d3374111758b99d99babd6dab802c56672a0b
Javier <javiantonini@gmail.com>
2019-03-17 12:49:55 +0000

    Using different approach to account link.

--- Customer/Block/Link.php
+++ Customer/Block/Link.php
@@ -2,24 +2,36 @@
 
 namespace Nomentu\Customer\Block;
 
+use Magento\Customer\Model\Context;
+
 class Link extends \Magento\Customer\Block\Account\Link
 {
-    protected $_customerSession;
+    protected $_httpContext;
 
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
-        \Magento\Customer\Model\Session $customerSession,
+        \Magento\Framework\App\Http\Context $httpContext,
         \Magento\Customer\Model\Url $customerUrl,
         array $data = []
      ) {
-         $this->_customerSession = $customerSession;
+         $this->_httpContext = $httpContext;
          parent::__construct($context, $customerUrl, $data);
      }
+
+     /**
+     * Is logged in
+     *
+     * @return bool
+     */
+    public function isLoggedIn()
+    {
+        return $this->_httpContext->getValue(Context::CONTEXT_AUTH);
+    }
 
     protected function _toHtml()
     {    
         $responseHtml = null; //  need to return at-least null
-        if($this->_customerSession->isLoggedIn()) {
+        if($this->isLoggedIn()) {
             $responseHtml = parent::_toHtml();
         }
         return $responseHtml;
